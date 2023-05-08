85d330614c3a066a8b605665765d3e4e2a408aab
Nguyen Vu <nguyenanhvu2172000@gmail.com>
2023-05-08 08:51:03 +0700

    search by name

--- src/content/applications/Movie/Movie.tsx
+++ src/content/applications/Movie/Movie.tsx
@@ -1,14 +1,16 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useCallback, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
-import { Card } from '@mui/material';
+import { Card, Box, TextField, debounce } from '@mui/material';
 
 import MovieTable from './MovieTable';
 import { QUERY_KEYS } from 'src/models/key';
 import { getMovieList } from 'src/utils/api/movie';
 import { IMovieListDataResponse } from 'src/models/api/movie.interface';
+import { useNavigate } from 'react-router';
 
 function Movie() {
+  const navigate = useNavigate();
   const [page, setPage] = useState<number>(0);
   const [limit, setLimit] = useState<number>(5);
   const [keyword, setKeyword] = useState<string>('');
@@ -38,8 +40,37 @@ function Movie() {
     setLimit(parseInt(event.target.value));
   };
 
+  const onChangeKeyword = (e: { target: { value: string } }) => {
+    debounceInput(e.target.value);
+  };
+
+  const debounceInput = useCallback(
+    debounce((keyword: string) => debounceKeyword(keyword), 1000),
+    []
+  );
+
+  const debounceKeyword = (keyword: string) => {
+    navigate({
+      pathname: '/book',
+      search: `?page=${page}&limit=${limit}&keyword=${keyword}`
+    });
+    setKeyword(keyword);
+  };
+
   return (
     <Card>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '75ch' }
+        }}
+        noValidate
+        autoComplete="off"
+        padding={3}
+        style={{ display: 'flex', alignItems: 'center' }}
+      >
+        <TextField id="outlined-required" onChange={onChangeKeyword} />
+      </Box>
       {movie && (
         <MovieTable
           page={page}
